82d61da8eec4b1c73707816a2d38fc3040f95ec7
leeedd <flavio_roberto_prado@hotmail.com>
2019-10-28 11:28:04 -0300

    Jogo Nave 2.0 - Som efeito adicionado

--- src/Jogo/Janelas.java
+++ src/Jogo/Janelas.java
@@ -1,7 +1,12 @@
 package Jogo;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.net.URL;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -15,46 +20,93 @@ import javax.swing.UIManager;
 public class Janelas extends JFrame {
 
 	public Janelas() {
-	
+
 		try {
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
 		} catch (Exception e) {
 			// erro
 		}
 
+		this.addWindowListener(new WindowListener() {
+
+			@Override
+			public void windowOpened(WindowEvent e) {
+				
+				play("HammerFall");
+			}
+
+			@Override
+			public void windowIconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
 		JMenuBar barraMenu = new JMenuBar();
 		JMenu file = new JMenu("File");
-		
-		
+
 		JMenuItem instrucoes = new JMenuItem("Instructions");
 		instrucoes.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				
-				JOptionPane.showMessageDialog(null, "Bem vindo ao Dead Space. \nDestrua os inimigos para sobreviver no espaço e voltar para casa. "
-						+ "\nComandos. Utilize as setas para desviar dos alienigênas."
-						+ "\nBarra de espaço faz a nave disparar o missel."
-						+ "\n'F' muda para 3 tipos de armas diferentes. Missel 1, Missel 2 e a Bomba Nuclear"
-						+ "\nDivirta-se. ", "Informações", JOptionPane.INFORMATION_MESSAGE);
+
+				JOptionPane.showMessageDialog(null,
+						"Bem vindo ao Dead Space. \nDestrua os inimigos para sobreviver no espaço e voltar para casa. "
+								+ "\nComandos. Utilize as setas para desviar dos alienigênas."
+								+ "\nBarra de espaço faz a nave disparar o missel."
+								+ "\n'F' muda para 3 tipos de armas diferentes. Missel 1, Missel 2 e a Bomba Nuclear"
+								+ "\nDivirta-se. ",
+						"Informações", JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
 
 		JMenuItem sobre = new JMenuItem("Info");
 		sobre.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				
-				JOptionPane.showMessageDialog(null, "Jogo desenvolvido por Flávio Prado!", "Informações", JOptionPane.INFORMATION_MESSAGE);
-				
+
+				JOptionPane.showMessageDialog(null, "Jogo desenvolvido por Flávio Prado!", "Informações",
+						JOptionPane.INFORMATION_MESSAGE);
+
 			}
 		});
 
 		JMenuItem sair = new JMenuItem("Exit");
-		
+
 		sair.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
@@ -65,11 +117,10 @@ public class Janelas extends JFrame {
 		file.add(sobre);
 		file.add(new JSeparator());
 		file.add(sair);
-		
+
 		barraMenu.add(file);
-		
+
 		setJMenuBar(barraMenu);
-		
 
 		add(new Fase());
 		setTitle("Dead Space");
@@ -78,6 +129,15 @@ public class Janelas extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setVisible(true);
+	}
+
+	public void play(String HammerFall) {
+
+		URL url = getClass().getResource(HammerFall + ".wav");
+		AudioClip audio = Applet.newAudioClip(url);
+		audio.play();
+		audio.loop();
+
 	}
 
 	public static void main(String[] args) {
